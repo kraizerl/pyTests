2a910e1351d7c457800b7f07d3ffdae8036ad8b3
l.Z <zaidowiczlorenzo@gmail.com>
2023-01-31 22:20:05 -0500

    added 2d lists tests
    created list added random numbers and found max very cool did it myself no help required

--- pyLists.py
+++ pyLists.py
@@ -1,1 +1,33 @@
 #Lists / Arrays 
+import random
+
+sets = ["R","E","Q","A","P","C"]
+
+sets.sort() #alphabetical sort
+#.clear() removes elements from list
+#.pop() remove last element
+#.append() adds new element
+
+for i in sets:
+    print(i, end=" ")
+print()
+#2d lists/ multidimensional arrays
+
+lunch = ["bread","corn"]
+drinks = ["water","soda"]
+dessert = ["cake","cok"]
+
+menu = [lunch,drinks,dessert]
+
+print
+
+for i in menu:
+    print(i)
+
+nums = []
+
+for i in range(10):
+    nums.insert(i,random.randrange(1,200))
+
+print(nums)
+print(max(nums))
